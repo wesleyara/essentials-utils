5162fea6cbe8c8963ee264a022479153e6c91341
wesleyara <wes1gom@gmail.com>
2022-11-29 11:07:21 -0300

    feat: add tests

---
+++ src/index.test.ts
@@ -0,0 +1,61 @@
+import { describe, expect, it } from "vitest";
+
+import {
+  upperFirst,
+  fetchRequest,
+  delay,
+  arrayEquals,
+  arrayToObject,
+  generateArray,
+  reverseString,
+} from "./index";
+
+describe("all methods in the package", () => {
+  it("capitalize the first letter of the string.", () => {
+    expect(upperFirst("foo")).toBe("Foo");
+  });
+
+  it("fetch data from an api.", async () => {
+    expect(
+      await fetchRequest("https://api.github.com/users/wesleyara"),
+    ).toEqual(
+      expect.objectContaining({
+        login: "wesleyara",
+      }),
+    );
+  });
+
+  it("delay the execution of a function.", async () => {
+    const start = Date.now();
+    await delay(1000);
+    const end = Date.now();
+    expect(end - start).toBeGreaterThanOrEqual(1000);
+  });
+
+  it("check if two arrays are equal.", () => {
+    expect(arrayEquals([1, 2, 3], [1, 2, 3])).toBe(true);
+  });
+
+  it("convert an array to an object.", () => {
+    expect(
+      arrayToObject(
+        [
+          { id: 1, name: "a" },
+          { id: 2, name: "b" },
+        ],
+        "id",
+      ),
+    ).toEqual({
+      1: { id: 1, name: "a" },
+      2: { id: 2, name: "b" },
+    });
+  });
+
+  it("generate an array of numbers.", () => {
+    expect(generateArray(5)).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it("reverse a string.", () => {
+    expect(reverseString("foo")).toBe("oof");
+  });
+});
