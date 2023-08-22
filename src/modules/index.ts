a09a7682a14a20ae6b4e1387a82902ee72630ecd
wesleyara <wes1gom@gmail.com>
2023-08-22 14:20:42 -0300

    feat: adding setStorage and getStorage method

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -149,3 +149,13 @@ export const generateNullArray = (quantity: number) => {
   }
   return arr;
 };
+
+export const setStorage = (key: string, value: any) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const getStorage = (key: string) => {
+  const data = localStorage.getItem(key);
+
+  return data ? JSON.parse(data) : undefined;
+};
