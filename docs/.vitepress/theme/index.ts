65dfe806e7660d19545e375d44aa2764d646a060
wesleyara <wes1gom@gmail.com>
2025-02-03 22:47:03 -0300

    feat: update docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './styles.css'
+import 'virtual:group-icons.css'
 
 export default {
   extends: DefaultTheme,
